ed45b280d5193643c7dcfd87f43d6e4ede2b0527
Nahom Ebssa <enahom99@outlook.com>
2021-07-21 13:19:37 -0400

    Additional documentation

--- Main.java
+++ Main.java
@@ -26,36 +26,72 @@ class Card {
     }
 }
 
-class CardDealer {
+/**
+ * A class to represent a deck of cards
+ */
+class CardDeck {
 
+    /**
+     * The cards in the deck
+     */
     private List<Card> cards;
+
+    /**
+     * The cards that were newly added
+     */
     private List<Card> addedCards;
+
+    /**
+     * The map of each phrase to its description
+     */
     private Map<String, String> phraseDescriptionMap;
+
+    /**
+     * A flag to remember if any cards were removed
+     */
     private boolean removedFlag;
 
-    CardDealer() {
+    CardDeck() {
         cards = new ArrayList<>();
         addedCards = new ArrayList<>();
         phraseDescriptionMap = new HashMap<>();
         removedFlag = false;
     }
 
+    /**
+     * Adds a card if it is not already in the deck
+     * @param card The card to add
+     */
     void addCard(Card card) {
+        // add the card to the map only if its not in the map
         if (phraseDescriptionMap.putIfAbsent(card.getPhrase(), card.getDescription()) == null) {
+            // also keep track of it in the list
             addedCards.add(card);
         } else {
             // card already exists
         }
     }
     
+    /**
+     * Removes a cards from the card deck
+     * @param card The card to remove
+     */
     void removeCard(Card card) {
+        // check if the card actually exists in the deck
         if (phraseDescriptionMap.containsKey(card.getPhrase())) {
+            // remove the card from the list and map, and note that
+            // there has been a card removed from the deck (see exportCards)
             cards.remove(card);
             phraseDescriptionMap.remove(card.getPhrase());
             removedFlag = true;
         }
     }
 
+    /**
+     * Reads the cards from a file and loads them into the list
+     * and map
+     * @param filename
+     */
     void loadCards(String filename) {
         try {
             File inputFile = new File(filename);
@@ -67,6 +103,9 @@ class CardDealer {
             String description = br.readLine();
             
             while (phrase != null) {
+
+                // make a new card out of each two lines (phrase and description)
+                // and put it in the map
 
                 cards.add(new Card(phrase, description));
                 phraseDescriptionMap.put(phrase, description);
@@ -85,6 +124,11 @@ class CardDealer {
         }
     }
 
+    /**
+     * Updates the file so that it has all the new cards and doesn't
+     * have the removed ones
+     * @param filename Which file to write the cards to
+    */
     void exportCards(String filename) {
         try {
             File inputFile = new File(filename);
@@ -97,6 +141,8 @@ class CardDealer {
                     bw.write(String.format("%s\n%s\n", card.getPhrase(), card.getDescription()));
                 }
             } else {
+                // re-create the file writer to overwrite the file, so that
+                // we exclude the removed cards
                 bw.close();
                 fileWriter = new FileWriter(inputFile, false);
                 bw = new BufferedWriter(fileWriter);
@@ -123,17 +169,21 @@ public class Main {
         
         String filename = "input.txt";
         
-        CardDealer dealer = new CardDealer();
+        // Load pre-made cards from a file
+        CardDeck dealer = new CardDeck();
         dealer.loadCards(filename);
         
         
-        dealer = new CardDealer();
+        // Make a new empty CardDealer
+        dealer = new CardDeck();
 
+        // Add some cards
         dealer.addCard(new Card("card A", "description of card A."));
         dealer.addCard(new Card("card B", "description of card B."));
         dealer.addCard(new Card("card C", "description of card C."));
         dealer.addCard(new Card("card D", "description of card D."));
 
+        // Export these new Cards to a file
         dealer.exportCards("input2.txt");
 
     }
