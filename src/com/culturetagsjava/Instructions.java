d86ca8cdb54efdf129cb9bd27cd72072db805ae5
ekinma99 <divinegrace38@gmail.com>
2021-08-06 11:41:01 -0400

    Add Instructions to Instructions.java

--- src/com/culturetagsjava/Instructions.java
+++ src/com/culturetagsjava/Instructions.java
@@ -2,4 +2,8 @@ package com.culturetagsjava;
 public class Instructions{
 System.out.println("#### Rules for Culture Tags ####");
 System.out.println("There's no limit to how many people are on each team but the teams have to be equal in number.");
-System.out.println("");
+System.out.println("You will be prompted to enter the number of people on each team, as well as the team members names.");
+System.out.println("The team who plays first will be given an acronym and hint.");
+System.out.println("Your team has to determine what the acronym stands for.");
+System.out.println("If your team is able to solve it, your team wins the game.");
+System.out.println("Otherwise, the other team gets to guess another acronym.");
